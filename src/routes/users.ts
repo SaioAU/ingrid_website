9ed3e4f86b2f9c87379ac987c333ab58293f92f5
jfulse <jorgen.fulsebakke@gmail.com>
2021-02-20 12:50:10 +0100

    :sparkles: Add route to update user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,58 +5,89 @@ import { User } from '../entities';
 
 const router = Router();
 
-type CreateUserBody = {email: string, name: string, password: string}
+const { BAD_REQUEST, NOT_FOUND, OK } = StatusCodes;
+
+type UserInput = {id?: number, email?: string, name?: string, password?: string}
 
 // curl http://localhost:3000/users/all
 router.get('/all', async (_: Request, res: Response): Promise<void> => {
   const users = await User.find();
 
-  res.status(StatusCodes.OK).json({ users });
+  res.status(OK).json({ users });
 });
 
 /*
-curl -XPOST -H "Content-Type: application/json" http://localhost:3000/users/create \
+curl -XPOST -H "Content-Type: application/json" http://localhost:3000/users \
 --data '{"email": "test@example.com", "name": "Newman", "password": "yolo1234"}'
 */
-router.post('/create', async (req: Request<GenericObject, GenericObject, CreateUserBody>, res: Response): Promise<void> => {
+router.post('/', async (req: Request<GenericObject, GenericObject, UserInput>, res: Response): Promise<void> => {
   const { email, name, password } = req.body;
 
   if (!email || !name || !password) {
-    res.status(StatusCodes.BAD_REQUEST).send('Missing email, name or password');
+    res.status(BAD_REQUEST).send('Missing email, name or password');
     return;
   }
 
-  const newUser = new User();
-  newUser.name = name;
-  newUser.email = email;
-  newUser.password = password; // TODO: Hash
-  await newUser.save();
+  const user = new User();
+  user.name = name;
+  user.email = email;
+  user.password = password; // TODO: Hash
+  await user.save();
 
-  res.status(StatusCodes.OK).json({});
+  res.status(OK).json(user);
 });
 
 /*
-curl -XPOST -H "Content-Type: application/json" http://localhost:3000/users/create \
---data '{"userId": 1}'
+curl -XPATCH -H "Content-Type: application/json" http://localhost:3000/users \
+--data '{"id": 1, "name": "new name"}'
 */
-router.post('/delete', async (req: Request<GenericObject, GenericObject, { userId: number }>, res: Response): Promise<void> => {
-  const { userId } = req.body;
+router.patch('/', async (req: Request<GenericObject, GenericObject, UserInput>, res: Response): Promise<void> => {
+  const {
+    id, email, name, password,
+  } = req.body;
 
-  if (!userId) {
-    res.status(StatusCodes.BAD_REQUEST).send('Missing userId');
+  if (!id) {
+    res.status(BAD_REQUEST).send('Missing id');
     return;
   }
 
-  const userToRemove = await User.findOne({ id: userId });
+  const user = await User.findOne({ id });
 
-  if (!userToRemove) {
-    res.status(StatusCodes.NOT_FOUND).send('User not found');
+  if (!user) {
+    res.status(NOT_FOUND).send('User not found');
     return;
   }
 
-  await userToRemove.remove();
+  user.name = name ?? user.name;
+  user.email = email ?? user.email;
+  user.password = password ?? user.password; // TODO: Hash
+  await user.save();
 
-  res.status(StatusCodes.OK).json({});
+  res.status(OK).json(user);
+});
+
+/*
+curl -XDELETE -H "Content-Type: application/json" http://localhost:3000/users \
+--data '{"id": 1}'
+*/
+router.delete('/', async (req: Request<GenericObject, GenericObject, UserInput>, res: Response): Promise<void> => {
+  const { id } = req.body;
+
+  if (!id) {
+    res.status(BAD_REQUEST).send('Missing id');
+    return;
+  }
+
+  const user = await User.findOne({ id });
+
+  if (!user) {
+    res.status(NOT_FOUND).send('User not found');
+    return;
+  }
+
+  await user.remove();
+
+  res.status(OK).json({ id });
 });
 
 export default router;
