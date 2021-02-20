00bd51e9dae5d95ca47a5a3d87f4d8e58fc7c74a
jfulse <jorgen.fulsebakke@gmail.com>
2021-02-20 12:38:15 +0100

    :sparkles: Add route to delete user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -19,17 +19,42 @@ curl -XPOST -H "Content-Type: application/json" http://localhost:3000/users/crea
 --data '{"email": "test@example.com", "name": "Newman", "password": "yolo1234"}'
 */
 router.post('/create', async (req: Request<GenericObject, GenericObject, CreateUserBody>, res: Response): Promise<void> => {
-  console.log('🚨', req.body);
   const { email, name, password } = req.body;
 
   if (!email || !name || !password) {
     res.status(StatusCodes.BAD_REQUEST).send('Missing email, name or password');
+    return;
   }
 
   const newUser = new User();
   newUser.name = name;
   newUser.email = email;
   newUser.password = password; // TODO: Hash
+  await newUser.save();
+
+  res.status(StatusCodes.OK).json({});
+});
+
+/*
+curl -XPOST -H "Content-Type: application/json" http://localhost:3000/users/create \
+--data '{"userId": 1}'
+*/
+router.post('/delete', async (req: Request<GenericObject, GenericObject, { userId: number }>, res: Response): Promise<void> => {
+  const { userId } = req.body;
+
+  if (!userId) {
+    res.status(StatusCodes.BAD_REQUEST).send('Missing userId');
+    return;
+  }
+
+  const userToRemove = await User.findOne({ id: userId });
+
+  if (!userToRemove) {
+    res.status(StatusCodes.NOT_FOUND).send('User not found');
+    return;
+  }
+
+  await userToRemove.remove();
 
   res.status(StatusCodes.OK).json({});
 });
